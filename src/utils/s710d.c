090cd8dced4adffbd45a81d65074284f128f3cf4
Ralf Horstmann <ralf.horstmann@gmx.de>
2010-01-10 22:30:39 +0100

    s710d: improve daemon mode, add debug mode
    - use daemon() to go to background
    - don't daemonize with -n
    - add verbosity level with -v
    - log to stderr in -n mode

--- src/utils/s710d.c
+++ src/utils/s710d.c
@@ -12,125 +12,141 @@
 
 /* globals */
 
-static int           gSigFlag;
+static volatile sig_atomic_t gSigFlag;
+static unsigned int          gDaemonize = 1;
+static unsigned int          gVerbose   = 0;
 
 /* function declarations */
 
-static void          signal_handler   ( int signum );
-
-/* usage */
-
-void usage() {
-    printf("usage: s710d [-h] [-d driver] [-f filedir] [device file]\n");
-	printf("       driver       may be either serial, ir, or usb\n");
-	printf("       device file  is required for serial and ir drivers.\n");
-	printf("       filedir      is the directory where output files are written to.\n");
-	printf("                    alternative is S710_FILEDIR environment variable.\n");
-}
+static void          s710d_signal_handler ( int signum );
+static void          s710d_log            (u_int level, const char *fmt, ...);
+static void          s710d_usage          ();
 
 /* main program */
 int
 main ( int argc, char **argv )
 {
-  pid_t           pid;
-  char            path[PATH_MAX];
-  char           *filedir = NULL;
-  files_t         file;
-  S710_Driver     driver;
-  int             ok;
-  const char     *driver_name = NULL;
-  const char     *device = NULL;
-  int             ch;
+	char            path[PATH_MAX];
+	char           *filedir = NULL;
+	files_t         file;
+	S710_Driver     driver;
+	const char     *driver_name = NULL;
+	const char     *device = NULL;
+	int             ch;
 
-  while ( (ch = getopt(argc,argv,"d:f:h")) != -1 ) {
-	  switch (ch) {
-	  case 'd':
-		  driver_name = optarg;
-		  break;
-	  case 'f':
-		  filedir = optarg;
-		  break;
-	  case 'h':
-		  usage();
-		  exit(0);
-		  break;
-	  default:
-		  usage();
-		  exit(1);
-	  }
-  }
-  argc -= optind;
-  argv += optind;
-  device = argv[0];
+	while ( (ch = getopt(argc,argv,"d:f:hnv")) != -1 ) {
+		switch (ch) {
+		case 'd':
+			driver_name = optarg;
+			break;
+		case 'f':
+			filedir = optarg;
+			break;
+		case 'h':
+			s710d_usage();
+			exit(0);
+			break;
+		case 'n':
+			gDaemonize = 0;
+			break;
+		case 'v':
+			gVerbose++;
+			break;
+		default:
+			s710d_usage();
+			exit(1);
+		}
+	}
+	argc -= optind;
+	argv += optind;
+	device = argv[0];
 
-  ok = driver_init (driver_name , device, &driver );
+	if (driver_init(driver_name, device, &driver) != 1) {
+		s710d_usage();
+		exit(1);
+	}
 
-  if ( ok != 1 ) {
-	  usage();
-	  exit(1);
-  }
+	if (gDaemonize) {
+		daemon(0, 0);
+		openlog("s710d", LOG_PID | LOG_NDELAY, LOG_USER);
+	}
 
-  if (filedir) {
-	  filedir = realpath(filedir, path);
-  } else if ((filedir = getenv("S710_FILEDIR")) != NULL) {
-	  filedir = realpath(filedir,path);
-  } else {
-	  filedir = S710_FILEDIR;
-  }
+	if (filedir) {
+		filedir = realpath(filedir, path);
+	} else if ((filedir = getenv("S710_FILEDIR")) != NULL) {
+		filedir = realpath(filedir,path);
+	} else {
+		filedir = S710_FILEDIR;
+	}
 
-  if (!filedir) {
-	  printf("could not resolve path. check S710_FILEDIR or -f\n");
-	  exit(1);
-  }
+	if (!filedir) {
+		s710d_log(0, "could not resolve path. check S710_FILEDIR or -f\n");
+		exit(1);
+	}
 
-  /* fork */
+	s710d_log(0,"started.\n");
 
-  if ( (pid = fork()) == 0 ) {
+	if ( driver_open ( &driver, S710_MODE_RDONLY ) < 0 ) {
+		s710d_log(0, "failed to initialize, aborting\n");
+		exit(1);
+	}
 
-    /* print syslog messages to stderr as well (debugging) */
+	/* install signal handlers */
+	signal(SIGINT, s710d_signal_handler);
+	signal(SIGTERM, s710d_signal_handler);
 
-    openlog("s710d",LOG_CONS|LOG_PID|LOG_PERROR,LOG_USER);
-    syslog(LOG_NOTICE,"started");
+	/* enter infinite loop */
+	while (!gSigFlag ) {
+		if (receive_file(&driver,&file,stderr)) {
+			save_files(&file,filedir,stderr);
+			print_files(&file,stderr); 
+		}      
+		usleep(10000);
+	}
 
-    if ( driver_open ( &driver, S710_MODE_RDONLY ) < 0 ) {
-      syslog(LOG_CRIT,"failed to initialize, aborting");
-      exit(1);
-    }
+	driver_close ( &driver );
 
-    /* install signal handlers */
+	/* note the signal */
+	s710d_log(0, "ended: received signal %d\n",gSigFlag);
 
-    signal(SIGINT,signal_handler);
-    signal(SIGTERM,signal_handler);
+	if (gDaemonize) {
+		closelog();   /* optional */
+	}
 
-    /* enter infinite loop */
-    
-    while ( !gSigFlag ) {
-
-      if ( receive_file(&driver,&file,stderr) ) {
-	save_files(&file,filedir,stderr);
-	print_files(&file,stderr); 
-      }      
-
-      usleep(10000);
-    }
-
-    driver_close ( &driver );
-
-    /* note the signal */
-
-    syslog(LOG_NOTICE,"ended: received signal %d",gSigFlag);
-    closelog();   /* optional */
-  }
-
-  return 0;
+	return 0;
 }
 
-
-
-/* signal handler just sets a global flag */
+static void
+s710d_usage()
+{
+    printf("usage: s710d [-h] [-n] [-d driver] [-f filedir] [device file]\n");
+	printf("       -d driver    may be either serial, ir, or usb\n");
+	printf("       -f filedir   is the directory where output files are written to\n");
+	printf("       -n           do not fork and log to stdout\n");
+	printf("       device file  is required for serial and ir drivers\n");
+	printf("                    alternative is S710_FILEDIR environment variable\n");
+}
 
 static void
-signal_handler ( int signum )
+s710d_log(u_int level, const char *fmt, ...)
 {
-  gSigFlag = signum;
+	va_list ap;
+
+	if (level > gVerbose)
+		return;
+
+	va_start(ap, fmt);
+	if (gDaemonize) {
+		vsyslog(LOG_INFO, fmt, ap);
+	} else {
+		vfprintf(stderr, fmt, ap);
+		fflush(stderr);
+	}
+	va_end(ap);
+}
+
+static void
+s710d_signal_handler ( int signum )
+{
+	gSigFlag = signum;
 }

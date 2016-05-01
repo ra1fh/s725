d4f0cbece50892fe4bb383fac7370fa658425730
Ralf Horstmann <ralf@ackstorm.de>
2016-05-01 20:06:18 +0200

    factor common code, minimize main function, copyright
    Since there is no code left from the upstream project
    in this particular file, adding a GPLv2+ copyright header
    like the rest of the code

--- s725get.c
+++ s725get.c
@@ -1,4 +1,22 @@
 
+/*
+ * Copyright (C) 2016  Ralf Horstmann
+
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 #include <ctype.h>
 #include <errno.h>
 #include <limits.h>
@@ -17,6 +35,10 @@
 #include "workout.h"
 #include "workout_print.h"
 
+static void write_hrm_data(BUF *files, const char *directory, int format_hrm, int format_raw);
+static void listen_bytes();
+static void write_bytes(const char *byte, int n);
+
 static void
 usage(void) {
 	printf("usage: s725get [-hHrv] [-b byte] [-d driver] [-D device] [-f directory]\n");
@@ -27,7 +49,7 @@ usage(void) {
 	printf("        -f directory   directory where output files are written to.\n");
 	printf("                       default: current working directory\n");
 	printf("        -l             listen for incoming data\n");
-	printf("        -L             listen for incoming bytes\n");
+	printf("        -L             listen for incoming bytes (for debugging)\n");
 	printf("        -t             get time\n");
 	printf("        -H             write HRM format\n");
 	printf("        -r             write raw srd format\n");
@@ -44,12 +66,8 @@ main(int argc, char **argv)
 	const char		 *opt_driver_name = NULL;
 	int				  opt_driver_type = DRIVER_IR;
 	const char		 *opt_device_name = NULL;
-	const char		 *suffix;
 	int				  opt_hrm = 0;
 	BUF				 *files;
-	BUF				 *buf;
-	workout_t		 *w;
-	int				  offset;
 	int				  opt_raw = 0;
 	int				  opt_time = 0;
 	int				  opt_user = 0;
@@ -57,11 +75,8 @@ main(int argc, char **argv)
 	int				  opt_listen_bytes = 0;
 	const char		 *opt_byte = NULL;
 	int				  opt_count = 1;
-	int				  count = 0;
 	int				  ch;
 	int				  ok;
-	time_t			  ft;
-	FILE*			  f;
 
 	snprintf(inipath, PATH_MAX, "%s/.s725rc", getenv("HOME"));
 	yyin = fopen(inipath, "r");
@@ -160,160 +175,143 @@ main(int argc, char **argv)
 		fatalx("unable to open port: %s", strerror(errno));
 
 	if (opt_byte) {
-		unsigned char mask = 128;
-		unsigned char out;
-		int c;
-		int i;
-		sscanf(opt_byte, "%u", &c);
-		out = (unsigned char) c;
-		buf = buf_alloc(0);
-		for (i = 0; i < opt_count; ++i)
-			buf_putc(buf, out);
-		if (log_get_level() > 0) {
-			log_writeln("byte: %hhu", out);
-			log_write("bits: ");
-			for (; mask; mask >>=1) {
-				log_write("%u", out & mask ? 1 : 0);
-			}
-			log_writeln("");
-		}
-		driver_write(buf);
+		write_bytes(opt_byte, opt_count);
 		driver_close();
-		exit(0);
+		return 0;
 	}
 
 	if (opt_listen_bytes) {
-		unsigned char byte;
-		while (1) {
-			if (driver_read_byte(&byte)) {
-				log_write("0x%02hhx %c\n", byte, byte);
-			}
-		}
+		listen_bytes();
+		driver_close();
+		return 0;
 	}
 	
 	if (opt_time) {
 		time_get();
 		driver_close();
-		exit(0);
+		return 0;
 	}
 
 	if (opt_user) {
 		user_get();
 		driver_close();
-		exit(0);
+		return 0;
 	}
 
 	files = buf_alloc(0);
 
-	if (opt_raw)
-		suffix = "srd";
-	else
-		if (opt_hrm)
-			suffix = "hrm";
-		else
-			suffix = "txt";
-
 	if (opt_listen) {
 		if (files_listen(files)) {
-			buf = buf_alloc(0);
-			offset = 0;
-			count = 0;
-			while (files_split(files, &offset, buf)) {
-				char tmbuf[128];
-				char fnbuf[BUFSIZ];
-
-				count++;
-				ft = files_timestamp(buf, 0);
-				strftime(tmbuf, sizeof(tmbuf), "%Y%m%dT%H%M%S", localtime(&ft));
-				snprintf(fnbuf, sizeof(fnbuf), "%s/%s.%05zd.%s",
-						 opt_directory_name, tmbuf, buf_len(buf), suffix);
-
-				if (opt_raw) {
-					f = fopen(fnbuf, "w");
-					if (f) {
-						log_writeln("File %02d: Saved as %s",
-									count, fnbuf);
-						fwrite(buf_get(buf), buf_len(buf), 1, f);
-						fclose(f);
-					} else {
-						log_writeln("File %02d: Unable to save %s: %s",
-									count, fnbuf, strerror(errno));
-					}
-				} else {
-					w = workout_read_buf(buf);
-					if (w) {
-						f = fopen(fnbuf, "w");
-						if (f) {
-							log_writeln("File %02d: Saved as %s", count, fnbuf);
-							if (opt_hrm)
-								workout_print_hrm(w, f);
-							else
-								workout_print_txt(w, f, S725_WORKOUT_FULL);
-							fclose(f);
-						} else {
-							log_writeln("File %02d: Unable to save %s: %s",
-										count, fnbuf, strerror(errno));
-						}
-						workout_free(w);
-					} else {
-						log_writeln("Failed to parse workout for %s", fnbuf);
-					}
-				}
-			}
-			log_writeln("Saved %d file%s", count, (count==1) ? "" : "s");
+			write_hrm_data(files, opt_directory_name, opt_hrm, opt_raw);
 		}
-		exit(0);
-	}
-	
-	if (files_get(files)) {
-		buf = buf_alloc(0);
-		offset = 0;
-		count = 0;
-		while (files_split(files, &offset, buf)) {
-			char tmbuf[128];
-			char fnbuf[BUFSIZ];
-
-			count++;
-			ft = files_timestamp(buf, 0);
-			strftime(tmbuf, sizeof(tmbuf), "%Y%m%dT%H%M%S", localtime(&ft));
-			snprintf(fnbuf, sizeof(fnbuf), "%s/%s.%05zd.%s",
-					 opt_directory_name, tmbuf, buf_len(buf), suffix);
-
-			if (opt_raw) {
-				f = fopen(fnbuf, "w");
-				if (f) {
-					log_writeln("File %02d: Saved as %s", count, fnbuf);
-					fwrite(buf_get(buf), buf_len(buf), 1, f);
-					fclose(f);
-				} else {
-					log_writeln("File %02d: Unable to save %s: %s",
-								count, fnbuf, strerror(errno));
-				}
-			} else {
-				w = workout_read_buf(buf);
-				if (w) {
-					f = fopen(fnbuf, "w");
-					if (f) {
-						log_writeln("File %02d: Saved as %s", count, fnbuf);
-						if (opt_hrm)
-							workout_print_hrm(w, f);
-						else
-							workout_print_txt(w, f, S725_WORKOUT_FULL);
-						fclose(f);
-					} else {
-						log_writeln("File %02d: Unable to save %s: %s",
-									count, fnbuf, strerror(errno));
-					}
-					workout_free(w);
-				} else {
-					log_writeln("Failed to parse workout for %s", fnbuf);
-				}
-			}
+	} else {
+		if (files_get(files)) {
+			write_hrm_data(files, opt_directory_name, opt_hrm, opt_raw);
 		}
-		log_writeln("Saved %d file%s", count, (count==1) ? "" : "s");
 	}
 
 	buf_free(files);
 	driver_close();
 	return 0;
+}
+
+static void
+listen_bytes()
+{
+	unsigned char byte;
+	while (1) {
+		if (driver_read_byte(&byte)) {
+			log_write("0x%02hhx %c\n", byte, byte);
+		}
+	}
+}
+
+static void
+write_bytes(const char *byte, int count)
+{
+	BUF	*buf;
+	unsigned char mask = 128;
+	unsigned char out;
+	int c;
+	int i;
+	sscanf(byte, "%u", &c);
+	out = (unsigned char) c;
+	buf = buf_alloc(0);
+	for (i = 0; i < count; ++i)
+		buf_putc(buf, out);
+	if (log_get_level() > 0) {
+		log_writeln("byte: %hhu", out);
+		log_write("bits: ");
+		for (; mask; mask >>=1) {
+			log_write("%u", out & mask ? 1 : 0);
+		}
+		log_writeln("");
+	}
+	driver_write(buf);
+}
+
+static void
+write_hrm_data(BUF *files, const char* directory, int format_hrm, int format_raw)
+{
+	const char *suffix;
+	workout_t *w;
+	FILE* f;
+	BUF *buf;
+	time_t ft;
+	int	offset;
+	int count;
+
+	if (format_raw)
+		suffix = "srd";
+	else
+		if (format_hrm)
+			suffix = "hrm";
+		else
+			suffix = "txt";
+
+	buf = buf_alloc(0);
+	offset = 0;
+	count = 0;
+	while (files_split(files, &offset, buf)) {
+		char tmbuf[128];
+		char fnbuf[BUFSIZ];
+
+		count++;
+		ft = files_timestamp(buf, 0);
+		strftime(tmbuf, sizeof(tmbuf), "%Y%m%dT%H%M%S", localtime(&ft));
+		snprintf(fnbuf, sizeof(fnbuf), "%s/%s.%05zd.%s",
+				 directory, tmbuf, buf_len(buf), suffix);
+
+		if (format_raw) {
+			f = fopen(fnbuf, "w");
+			if (f) {
+				log_writeln("File %02d: Saved as %s", count, fnbuf);
+				fwrite(buf_get(buf), buf_len(buf), 1, f);
+				fclose(f);
+			} else {
+				log_writeln("File %02d: Unable to save %s: %s",
+							count, fnbuf, strerror(errno));
+			}
+		} else {
+			w = workout_read_buf(buf);
+			if (w) {
+				f = fopen(fnbuf, "w");
+				if (f) {
+					log_writeln("File %02d: Saved as %s", count, fnbuf);
+					if (format_hrm)
+						workout_print_hrm(w, f);
+					else
+						workout_print_txt(w, f, S725_WORKOUT_FULL);
+					fclose(f);
+				} else {
+					log_writeln("File %02d: Unable to save %s: %s",
+								count, fnbuf, strerror(errno));
+				}
+				workout_free(w);
+			} else {
+				log_writeln("Failed to parse workout for %s", fnbuf);
+			}
+		}
+	}
+	log_writeln("Saved %d file%s", count, (count==1) ? "" : "s");
 }

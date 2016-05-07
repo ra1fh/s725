4aead7bed27c6bf8209a3d2ce3f3d8d2446b86c8
Ralf Horstmann <ralf@ackstorm.de>
2016-05-07 10:09:00 +0200

    Add missing driver struct pieces for previous commit

--- driver_int.h
+++ driver_int.h
@@ -12,16 +12,18 @@
 struct s725_driver_ops;
 
 struct s725_driver {
-	char path[PATH_MAX];
-	void *data;
 	struct s725_driver_ops *dops;
+	void *data;
+	int uses_frames;
+	char path[PATH_MAX];
 };
 
 struct s725_driver_ops {
-	int (*init)  (struct s725_driver* d);
-	int (*read)  (struct s725_driver* d, unsigned char *byte);
-	int (*write) (struct s725_driver* d, BUF *buf);
-	int (*close) (struct s725_driver* d);
+	int (*init)      (struct s725_driver* d);
+	int (*read)      (struct s725_driver* d, BUF *buf);
+	int (*read_byte) (struct s725_driver* d, unsigned char *byte);
+	int (*write)     (struct s725_driver* d, BUF *buf);
+	int (*close)     (struct s725_driver* d);
 };
 
 #endif	/* DRIVER_H */
